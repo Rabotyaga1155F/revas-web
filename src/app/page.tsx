6491e644505a40e077d69cb3f04a0b1c469a4d2f
Rabotyaga1155 <cool.poryadin2014@yandex.ru>
2024-03-05 20:44:24 +0500

    Added downheader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+import header from "@/assets/images/header.jpg";
 
 export default function Home() {
   return (
     <div className={"mx-60"}>
       <Header />
+      <Image className={"w-full h-[210px]"} src={header} alt={"DownHeader"} />
       <main></main>
       <Footer />
     </div>
