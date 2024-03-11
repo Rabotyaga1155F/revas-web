5afeae76dba0cd3d0c83b1881a936a557396af95
Andrey <cool.poryadin2014@yandex.ru>
2024-03-11 20:18:53 +0500

    Added sendMail

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { sendMail } from "@/utils/mail";
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const { Name, Phone } = req.body;
-
-  sendMail("Тестовое письмо");
-}
